94a599d9cb5cefbcfabc8b472eb19b115b9d967a
bannt <trongbannt@gmail.com>
2019-05-12 14:34:36 +0700

    update code  Flexible Lists

--- my-app/src/MainConcepts/ListState.js
+++ my-app/src/MainConcepts/ListState.js
@@ -1,14 +1,31 @@
-import React, { Component } from "react";
-import Person from "./Person";
-import "./css/ListState.css"
+import React, { Component } from 'react';
+import Person from './Person';
+import './css/ListState.css'
 
 class ListState extends Component {
     state = {
-        persons: [{ name: "Person_1", age: 25 }, { name: "Person_1", age: 25 }, { name: "Person_1", age: 25 }]
+        persons: [
+            { id: 'p1', name: 'Person_1', age: 25 },
+            { id: 'p2', name: 'Person_1', age: 25 },
+            { id: 'p3', name: 'Person_1', age: 25 }
+        ]
     }
 
+    changeNameHandler = (event, id) => {
+        debugger;
+        const personIndex = this.state.persons.findIndex(p => { return p.id === id });
+        const person = { ...this.state.persons[personIndex] };
+        person.name = event.target.value;
+        const personsTemp = [...this.state.persons]
+        personsTemp[personIndex] = person;
+
+        this.setState({
+            persons: personsTemp
+        });
+    };
+
     deletePersonHandler = (personIndex) => {
-        let tempPersons = this.state.persons;
+        let tempPersons = [...this.state.persons];
         tempPersons.splice(personIndex, 1);
         this.setState({
             persons: tempPersons
@@ -16,17 +33,26 @@ class ListState extends Component {
     }
 
     render() {
-        return (
-            <React.Fragment>
+        const person = (
+            <div>
                 {this.state.persons.map((i, index) => {
                     return (
-                        <React.Fragment>
-                            <Person name={i.name} age={i.age} />
-                            <button type="button" className="btn btn-danger btn-circle" onClick={() => this.deletePersonHandler(index)}>X</button>
-                        </React.Fragment>
+                        <div key={i.id}>
+                            <Person
+                                name={i.name}
+                                age={i.age}
+                                changeName={(event, id) => { this.changeNameHandler(event, i.id) }}
+                            />
+                            <button type='button' className='btn btn-danger btn-circle' onClick={() => this.deletePersonHandler(index)}>X</button>
+                        </div>
                     )
                 })}
+            </div>
+        );
 
+        return (
+            <React.Fragment>
+                {person}
             </React.Fragment>
         );
     }
