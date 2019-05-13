d017dee33f015ec7d10182f5eaf1a998c4633deb
bannt <ban.nguyen@ntq-solution.com.vn>
2019-05-13 16:30:47 +0700

    update hookjs

--- my-app/src/MainConcepts/Hook.js
+++ my-app/src/MainConcepts/Hook.js
@@ -1,34 +1,79 @@
 import React, { useState } from "react";
-import Person from "./Person";
 import { Button } from "react-bootstrap"
+import Person from "./Person";
+import "./css/Hook.css"
 
 const hook = () => {
 
     const [personState, setPersonState] = useState({
         persons: [
             { name: "Jack jons", age: 25 },
-            { name: "Mack Lauton", age: 29 },
-        ]
+        ],
+        togglePerson: false,
+        nameToggle: "Show"
     });
 
-    const switchNameHandler = () => {
+    const changeNameHandler = (event) => {
         setPersonState({
             persons: [
-                { name: "Jack jons", age:  personState.persons[0].age+1 },
-                { name: "Mack Lauton", age:  personState.persons[1].age+1 },
-            ]
+                { name: event.target.value, age: personState.persons[0].age},
+            ],
+            togglePerson: true,
+            nameToggle: personState.nameToggle
         });
+      };
+
+    const switchNameHandler = () => {
+        if (personState.togglePerson === true) {
+            setPersonState({
+                persons: [
+                    { name: "Jack jons", age: personState.persons[0].age + 1 },
+                ],
+                togglePerson: true,
+                nameToggle: personState.nameToggle
+            });
+        }
+    }
+
+    const TogglePersonHandler = () => {
+        const currentValue = personState.togglePerson;
+        const nameToggleCurrent = personState.nameToggle === "Show" ? "Hidden" : "Show";
+        setPersonState({
+            persons: [
+                { name: "Jack jons", age: personState.persons[0].age },
+            ],
+            togglePerson: !currentValue,
+            nameToggle: nameToggleCurrent
+        });
+    }
+
+    let paragraph = null;
+    if (personState.togglePerson === true) {
+        paragraph = (
+            <div className="panel">
+                <h4>What's this course about?</h4>
+                <p>Learn React or dive deeper into it. Learn the theor, solve assignments,
+                    practice in demo projects and build one big application which is improved
+        throughout the course: The Burger Builder!</p>
+            </div>
+        )
     }
 
     return (
         <React.Fragment>
-            <Person 
-                name={personState.persons[0].name} 
-                age={personState.persons[0].age}
-            >
-            </Person>
-            <Person name={personState.persons[1].name} age={personState.persons[1].age}></Person>
+            {personState.togglePerson === true ?
+                <div>
+                    <Person
+                        name={personState.persons[0].name}
+                        age={personState.persons[0].age}
+                        changeName = {changeNameHandler}
+                    >
+                    </Person>
+                </div> : null
+            }
+            {paragraph}
             <Button variant="success" onClick={switchNameHandler}>Switch name</Button>
+            <Button variant="info" onClick={TogglePersonHandler}>{personState.nameToggle}</Button>
         </React.Fragment>
     );
 }
